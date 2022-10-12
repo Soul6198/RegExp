e5b56b8d7f632567c81afb1a1669fd292a73c3ab
Alexandr_Popov <Reorin262@gmail.com>
2022-10-12 19:15:55 +0300

    FINAL

--- Regular_expressions.py
+++ Regular_expressions.py
@@ -4,68 +4,42 @@ import csv
 with open("phonebook_raw.csv", 'r', encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-    new_list = []
+    with open("phonebook_raw.csv", encoding="utf-8") as f:
+        rows = csv.reader(f, delimiter=",")
+        contact_list = list(rows)
 
+    pattern = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
+    substitution = r'+7(\2)-\3-\4-\5 \6\7'
+    step_list = list()
+    correct_list = list()
 
-def names_moving():
-    name_pattern = r'([А-Я])'
-    name_substitution = r' \1'
-    for column in contacts_list[1:]:
-        line = column[0] + column[1] + column[2]
-        if len((re.sub(name_pattern, name_substitution, line).split())) == 3:
-            column[0] = re.sub(name_pattern, name_substitution, line).split()[0]
-            column[1] = re.sub(name_pattern, name_substitution, line).split()[1]
-            column[2] = re.sub(name_pattern, name_substitution, line).split()[2]
-        elif len((re.sub(name_pattern, name_substitution, line).split())) == 2:
-            column[0] = re.sub(name_pattern, name_substitution, line).split()[0]
-            column[1] = re.sub(name_pattern, name_substitution, line).split()[1]
-            column[2] = ''
-        elif len((re.sub(name_pattern, name_substitution, line).split())) == 1:
-            column[0] = re.sub(name_pattern, name_substitution, line).split()[0]
-            column[1] = ''
-            column[2] = ''
-    return
+    for item in contact_list:
+        fio = ' '.join(item[:3]).split(' ')
+        lastname = fio[0]
+        firstname = fio[1]
+        surname = fio[2]
+        organization = item[3]
+        position = item[4]
+        phone = re.sub(pattern, substitution, item[5])
+        email = item[6]
+        step_list.append([lastname, firstname, surname, organization, position, phone, email])
 
+    for item in step_list:
+        for step in step_list:
+            if item[0] == step[0] and item[1] == step[1]:
+                if item[2] == '':
+                    item[2] = step[2]
+                if item[3] == '':
+                    item[3] = step[3]
+                if item[4] == '':
+                    item[4] = step[4]
+                if item[5] == '':
+                    item[5] = step[5]
+                if item[6] == '':
+                    item[6] = step[6]
+        if item not in correct_list:
+            correct_list.append(item)
 
-def phone_number_formatting():
-    phone_pattern = re.compile(
-        r'(\+7|8)?\s*\(?(\d{3})\)?\s*\D?(\d{3})[-\s+]?(\d{2})-?(\d{2})((\s)?\(?(доб.)?\s?(\d+)\)?)?')
-    phone_substitution = r'+7(\2)\3-\4-\5\7\8\9'
-    for column in contacts_list:
-        column[5] = phone_pattern.sub(phone_substitution, column[5])
-    return
-
-
-def duplicates_combining():
-    for column in contacts_list[1:]:
-        first_name = column[0]
-        last_name = column[1]
-        for contact in contacts_list:
-            new_first_name = contact[0]
-            new_last_name = contact[1]
-            if first_name == new_first_name and last_name == new_last_name:
-                if column[2] == '':
-                    column[2] = contact[2]
-                if column[3] == '':
-                    column[3] = contact[3]
-                if column[4] == '':
-                    column[4] = contact[4]
-                if column[5] == '':
-                    column[5] = contact[5]
-                if column[6] == '':
-                    column[6] = contact[6]
-
-    for contact in contacts_list:
-        if contact not in new_list:
-            new_list.append(contact)
-    return new_list
-
-
-if __name__ == '__main__':
-    names_moving()
-    phone_number_formatting()
-    duplicates_combining()
-
-    with open("phonebook.csv", "w", encoding='utf-8') as f:
+    with open("phonebook.csv", "w", encoding="utf-8") as f:
         datawriter = csv.writer(f, delimiter=',')
-        datawriter.writerows(new_list)
+        datawriter.writerows(correct_list)
